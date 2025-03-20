8cdc08a6d4ceb72f1778ea571cac5cebb62b1774
llumnee <longo.luisaa@gmail.com>
2025-03-19 21:58:02 -0500

    Update super_trunfo.c (desafio nível novato)

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,7 +2,9 @@
 
 int main() {
 
-    char nome1[30], nome2[30];
+    char nomepais1[30], nomepais2[30];
+    char nomecidade1[30], nomecidade2[30];
+    char nomeestado1[30], nomeestado2[30];
     unsigned long int populacao1, populacao2;
     int pontos1, pontos2;
     float area1, area2;
@@ -12,11 +14,18 @@ int main() {
     int superpoder1, superpoder2;
     int comparacao1,  comparacao2, comparacao3, comparacao4, comparacao5, comparacao6;
 
-    //inserção de dados do primeiro país
-    //nome
-    printf("Digite o nome do primeiro país: ");
-    scanf("%s", nome1);
+    //inserção de dados da carta 1
+    //nome do país
+    printf("Digite o nome do país: ");
+    scanf("%s", nomepais1);
 
+    //nome do estado
+    printf("Digite o nome do estado: ");
+    scanf("%s", nomeestado1);
+
+    //nome da cidade
+    printf("Digite o nome da cidade: ");
+    scanf("%s", nomecidade1);
     //população
     printf("Digite a quantidade de habitantes: ");
     scanf("%lu", &populacao1);
@@ -33,10 +42,18 @@ int main() {
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontos1);
 
-    //inserção de dados do segundo país
-    //nome
-    printf("Digite o nome do segundo país: ");
-    scanf("%s", nome2);
+    //inserção de dados da carta 2
+    //nome do país
+    printf("Digite o nome do país: ");
+    scanf("%s", nomepais2);
+
+    //nome do estado
+    printf("Digite o nome do estado: ");
+    scanf("%s", nomeestado2);
+
+    //nome da cidade
+    printf("Digite o nome da cidade: ");
+    scanf("%s", nomecidade2);
 
     //população
     printf("Digite a quantidade de habitantes: ");
@@ -55,14 +72,14 @@ int main() {
     scanf("%d", &pontos2);
 
     //cálculos de densidade e pib per capta 
-    densidade1 = populacao1 / area1;
-    pibpercapta1 = pib1 / populacao1;
-    densidade2 = populacao2 / area2;
-    pibpercapta2 = pib2 / populacao2;
+    densidade1 = (float) populacao1 / area1;
+    pibpercapta1 = pib1 / (float) populacao1;
+    densidade2 = (float) populacao2 / area2;
+    pibpercapta2 = pib2 / (float) populacao2;
 
     //cálculo dos super poderes
-    superpoder1 = populacao1 + area1 + pib1 - densidade1;
-    superpoder2 = populacao2 + area2 + pib2 - densidade2;
+    superpoder1 = (float) populacao1 + area1 + pib1 - densidade1;
+    superpoder2 = (float) populacao2 + area2 + pib2 - densidade2;
 
     //comparação das cartas
     comparacao1 = area1 > area2;
@@ -72,14 +89,16 @@ int main() {
     comparacao5 = pontos1 > pontos2;
     comparacao6 = superpoder1 > superpoder2;
 
-    //exibição de resultados
-    printf("Comparação de Cartas:\n");
-    printf("Área: %d\n", comparacao1);
-    printf("População: %d\n", comparacao2);
-    printf("PIB: %d\n", comparacao3);
-    printf("Densidade: %d\n", comparacao4);
-    printf("Pontos turísticos: %d\n", comparacao5);
-    printf("Super Poder: %d\n", comparacao6);
+    //exibição da comparação
+    printf("Área da carta 1 - %s: %.0f\n", nomepais1, area1);
+    printf("Área da carta 2 - %s: %.0f\n", nomepais2, area2);
+
+    //carta vencedora
+    if (area1 > area2) {
+        printf("A carta 1 (%s) venceu!", nomepais1);
+    } else {
+        printf("A carta 2 (%s) venceu!", nomepais2);
+    }
         
 return 0;
     
